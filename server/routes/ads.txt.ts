c9defeeb2fea541d50ea7d68a4435f0aade6c9ff
bader <baidsforbusiness@gmail.com>
2025-10-22 10:50:12 +0300

    fix: used type/plain instead of html for ads.txt.ts served from nitro!

--- server/routes/ads.txt.ts
+++ server/routes/ads.txt.ts
@@ -1,5 +1,6 @@
-export default defineEventHandler(() => {
+export default defineEventHandler((event) => {
   // You can generate or fetch the content dynamically here
   // return "google.com, pub-GOOGLE_ADSENSE_ID, DIRECT, f08c47fec0942fa0";
+  setResponseHeader(event, 'Content-Type', 'text/plain; charset=utf-8')
   return ".";
 });
